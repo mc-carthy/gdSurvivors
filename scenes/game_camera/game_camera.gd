ff57a34fa7511a5219669087ab6a6efae328845d
Mick McCarthy <mick@mccarthy.tech>
2023-08-10 22:02:53 +0100

    Setup camera

---
+++ scenes/game_camera/game_camera.gd
@@ -0,0 +1,21 @@
+extends Camera2D
+
+const lerp_speed = 10
+
+
+var target_position = Vector2.ZERO
+
+
+func _ready() -> void:
+	make_current()
+
+
+func _process(delta: float) -> void:
+	acquire_target()
+	global_position = global_position.lerp(target_position, 1 - exp(-delta * lerp_speed))
+
+func acquire_target() -> void:
+	var player_nodes = get_tree().get_nodes_in_group('player')
+	if player_nodes.size() > 0:
+		var player = player_nodes[0] as Node2D
+		target_position = player.global_position
