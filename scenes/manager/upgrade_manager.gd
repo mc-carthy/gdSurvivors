e12df28a62538a445e9e7e613847a5d3f1683d54
Mick McCarthy <mick@mccarthy.tech>
2023-08-24 10:49:23 +0100

    Add ability acquisition logic

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const NUM_UPGRADES_OFFERED = 2
+
 @export var upgrade_pool: Array[AbilityUpgrade]
 @export var experience_manager: Node
 @export var upgrade_screen_scene: PackedScene
@@ -9,15 +11,6 @@ var current_upgrades = {}
 func _ready() -> void:
 	experience_manager.level_up.connect(on_level_up)
 
-
-func on_level_up(_level: int) -> void:
-	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
-	if chosen_upgrade == null: return
-	
-	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
-	add_child(upgrade_screen_instance)
-	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
-	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
 
 func apply_upgrade(upgrade: AbilityUpgrade) -> void:
 	var has_upgrade = current_upgrades.has(upgrade.id)
@@ -29,6 +22,24 @@ func apply_upgrade(upgrade: AbilityUpgrade) -> void:
 	else:
 		current_upgrades[upgrade.id]['quantity'] += 1
 	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
+
+func pick_upgrades() -> Array[AbilityUpgrade]:
+	var chosen_upgrades: Array[AbilityUpgrade] = []
+	var filtered_upgrades = upgrade_pool.duplicate()
+	for i in NUM_UPGRADES_OFFERED:
+		var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
+		chosen_upgrades.append(chosen_upgrade)
+		filtered_upgrades = filtered_upgrades.filter(func (upgrade): return upgrade.id != chosen_upgrade.id)
+	
+	return chosen_upgrades
+
+
+func on_level_up(_level: int) -> void:
+	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
+	add_child(upgrade_screen_instance)
+	var chosen_upgrades = pick_upgrades()
+	upgrade_screen_instance.set_ability_upgrades(chosen_upgrades as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
 
 
 func on_upgrade_selected(upgrade: AbilityUpgrade) -> void:
