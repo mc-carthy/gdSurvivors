e0a3fbc0af857e6c52951f41d7b41547097b7d5b
Mick McCarthy <mick@mccarthy.tech>
2023-09-02 14:40:18 +0100

    Add player movement speed upgrade

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -1,19 +1,18 @@
 extends CharacterBody2D
 
-const MAX_SPEED: float = 125;
-const ACCELERATION_SMOOTHING: float = 25
-
 @onready var damage_interval_timer = $DamageIntervalTimer
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var health_bar: ProgressBar = $HealthBar
 @onready var abilities = $Abilities
 @onready var animiation_player = $AnimationPlayer
 @onready var visuals: Node2D = $Visuals
+@onready var velocity_component: Node = $VelocityComponent
 
-var number_colliding_bodies = 0
-
+var number_colliding_bodies: int = 0
+var base_speed: float = 0
 
 func _ready() -> void:
+	base_speed = velocity_component.max_speed
 	$CollisionArea2D.body_entered.connect(on_body_entered)
 	$CollisionArea2D.body_exited.connect(on_body_exited)
 	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
@@ -25,12 +24,8 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var movement_vector: Vector2 = get_movement_vector()
 	var direction = movement_vector.normalized()
-	
-	var target_velocity = direction * MAX_SPEED
-	
-	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
-	
-	move_and_slide()
+	velocity_component.accelerate_in_direction(direction)
+	velocity_component.move(self)
 	
 	if movement_vector.length() > 0:
 		animiation_player.play('walk')
@@ -76,8 +71,9 @@ func on_health_changed() -> void:
 	update_health_display()
 
 
-func on_ability_upgrade_added(ability_upgrade: AbilityUpgrade, _current_upgrades: Dictionary) -> void:
-	if !ability_upgrade is Ability: return
-	
-	var ability = ability_upgrade as Ability
-	$Abilities.add_child(ability.ability_controller_scene.instantiate())
+func on_ability_upgrade_added(ability_upgrade: AbilityUpgrade, current_upgrades: Dictionary) -> void:
+	if ability_upgrade is Ability:
+		var ability = ability_upgrade as Ability
+		$Abilities.add_child(ability.ability_controller_scene.instantiate())
+	elif ability_upgrade.id == 'player_speed':
+		velocity_component.max_speed = base_speed + (base_speed + current_upgrades['player_speed']['quantity'] * 0.1)
